5ffed536a602ee6ee3f2257625038e4392089873
aprendiz_sistemas <aprendiz_sistemas@S10APRSIS1>
2015-09-10 15:40:50 -0500

    se cambio  tamaño al filtro de busqueda
    se centro el filtro de busqueda

--- view/default/indexTemplate.html.php
+++ view/default/indexTemplate.html.php
@@ -19,23 +19,17 @@ use mvc\i18n\i18nClass as i18n ?>
         </div>
         <div class="busqueda">
             <div class="row">
-                <div class="col-md-4 col-md-offset-3">
+                <div class="col-md-6 col-md-offset-3">
                     <form action="" class="search-form">
                         <div class="form-group has-feedback">
                             <label for="search" class="sr-only">Search</label>
-                            <input type="text" class="form-control" name="search" id="search" placeholder="search">
+                            <input type="text" class="form-control" name="search" id="search" placeholder="Busqueda...." required autofocus>
                             <span class="glyphicon glyphicon-search form-control-feedback"></span>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-
-
-
-
-
-
 
         <table class="table table-bordered table-responsive table-striped table-hover">
             <thead>
