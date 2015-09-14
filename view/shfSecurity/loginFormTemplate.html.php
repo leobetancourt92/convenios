9c02dc06841b91a227fe2b3e18ecd580c342cca9
aprendiz_sistemas <aprendiz_sistemas@S10APRSIS1>
2015-09-14 14:46:56 -0500

    se modifico el login
    iconos y tamaño de login fueron modificados

--- view/shfSecurity/loginFormTemplate.html.php
+++ view/shfSecurity/loginFormTemplate.html.php
@@ -2,21 +2,32 @@
 <?php use mvc\view\viewClass as view ?>
 <?php use mvc\session\sessionClass as session ?>
 <div class="container container-fluid">
-
-  <form class="form-signin" role="form" action="<?php echo routing::getInstance()->getUrlWeb('shfSecurity', 'login') ?>" method="POST">
-    <h2 class="form-signin-heading">Convenios Angel</h2>
-    <div class="form-group">
-	<label for="inputUser">Usuario:</label>
-	<input type="text" id="inputUser" name="inputUser" class="form-control" placeholder="Usuario" required autofocus>
-    </div>
-    <div class="form-group">
-	<label for="inputPassword">Contraseña: </label>
-	<input type="password" id="inputPassword" name="inputPassword" class="form-control" placeholder="Contraseña" required>
-    </div>
-    <button class="btn btn-lg btn-success" type="submit">Login</button>
-    <?php if (session::getInstance()->hasError() or session::getInstance()->hasInformation() or session::getInstance()->hasSuccess() or session::getInstance()->hasWarning()): ?>
-    <?php view::includeHandlerMessage() ?>
-    <?php endif ?>
-  </form>
-
+    
+    <form class="form-horizontal" role="form" action="<?php echo routing::getInstance()->getUrlWeb('shfSecurity', 'login') ?>" method="POST">
+        <h2 class="form-signin-heading">Convenios Angel</h2>
+        
+        <div class="form-group">
+            <label for="inputEmail" class="control-label col-xs-3"><i class="fa fa-user-md fa-2x"></i>    Usuario:</label>
+            <div class="col-xs-8">
+                <input type="text" id="inputUser" name="inputUser" class="form-control" placeholder="Usuario" required autofocus>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="inputPassword" class="control-label col-xs-3"><i class="fa fa-key fa-lg"></i>Password</label>
+            <div class="col-xs-8">
+                <input type="password" id="inputPassword" name="inputPassword" class="form-control" placeholder="Contraseña" required>
+            </div>
+        </div>
+       
+        <div class="form-group">
+            <div class="col-lg-12">
+                <button class="btn btn-lg btn-primary" type="submit">Login</button>
+            </div>
+        </div>
+        
+        <?php if (session::getInstance()->hasError() or session::getInstance()->hasInformation() or session::getInstance()->hasSuccess() or session::getInstance()->hasWarning()): ?>
+                <?php view::includeHandlerMessage() ?>
+            <?php endif ?>
+        
+    </form>
 </div> <!-- /container -->
