d4de341606996f486a3fb4d944a4153888d73ec0
practicante_dokeos <practicante_dokeos@172.31.5.136>
2015-09-22 09:38:59 -0500

    validador xss
    especificacion de funciones de la clase.

--- libs/vendor/xssValidatorClass.php
+++ libs/vendor/xssValidatorClass.php
@@ -26,32 +26,36 @@ namespace mvc\xss {
         }
 
         static public function filterXSS($val) {
-            // remove all non-printable characters. CR(0a) and LF(0b) and TAB(9) are allowed  
-            // this prevents some character re-spacing such as <java\0script>  
-            // note that you have to handle splits with \n, \r, and \t later since they *are* allowed in some inputs  
+            // remover todos  los caracteres que no se pueden imprimir en pantalla. CR(0a) y LF(0b) y TAB(9) son permitidos 
+            // esto previene algunos caracteres tales como el introducir espacios tales como <java\0script>  
+            // note que  usted tiene  que manejar separadores como  \n, \r, y \t despues ya que since ellos *son* permitidos en algunas entradas  
             $val = preg_replace('/([\x00-\x08][\x0b-\x0c][\x0e-\x20])/', '', $val);
 
-            // straight replacements, the user should never need these since they're normal characters  
-            // this prevents like <img src="@avascript:alert('XSS')">  
+              
+            // esto perviene  ataques e inserciones tales como <img src="@avascript:alert('XSS')">  
             $search = 'abcdefghijklmnopqrstuvwxyz';
             $search .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
             $search .= '1234567890!@#$%^&*()';
             $search .= '~`";:?+/={}[]-_|\'\\';
             for ($i = 0; $i < strlen($search); $i++) {
-                // ;? matches the ;, which is optional  
-                // 0{0,7} matches any padded zeros, which are optional and go up to 8 chars  
-                // @ @ search for the hex values  
+                // ;? coincidencias ;, opcional  
+                // 0{0,7} coincidencias con ceros, son opcionales y para cadenas superiores a 8 caracteres  
+                // @ @ busqueda de valores en hexadecimal  
                 $val = preg_replace('/(&#[x|X]0{0,8}' . dechex(ord($search[$i])) . ';?)/i', $search[$i], $val); // with a ;  
-                // @ @ 0{0,7} matches '0' zero to seven times  
-                $val = preg_replace('/(�{0,8}' . ord($search[$i]) . ';?)/', $search[$i], $val); // with a ;  
+                // @ @ 0{0,7} encontrar el valor '0'  siete veces en la cadena de  caracteres.
+                $val = preg_replace('/(�{0,8}' . ord($search[$i]) . ';?)/', $search[$i], $val); // con un  ';'  
             }
 
-            // now the only remaining whitespace attacks are \t, \n, and \r  
+            
+
+
+           //ahora solo faltan los espacios en blanco  con    are \t, \n, and \r 
+            
             $ra1 = Array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'style', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'title', 'base');
             $ra2 = Array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload');
             $ra = array_merge($ra1, $ra2);
 
-            $found = true; // keep replacing as long as the previous round replaced something  
+            $found = true;  
             while ($found == true) {
                 $val_before = $val;
                 for ($i = 0; $i < sizeof($ra); $i++) {
@@ -66,10 +70,10 @@ namespace mvc\xss {
                         $pattern .= $ra[$i][$j];
                     }
                     $pattern .= '/i';
-                    $replacement = substr($ra[$i], 0, 2) . '<x>' . substr($ra[$i], 2); // add in <> to nerf the tag  
-                    $val = preg_replace($pattern, $replacement, $val); // filter out the hex tags  
+                    $replacement = substr($ra[$i], 0, 2) . '<x>' . substr($ra[$i], 2); // agrega  en  <> para debilitar la etiqueta 
+                    $val = preg_replace($pattern, $replacement, $val); // filtrado de los valores en hexadecimal  
                     if ($val_before == $val) {
-                        // no replacements were made, so exit the loop  
+                        // si  el filtro no se activa por ninguna de las  condiciones ,retorna un falso y se termina el ciclo de verificación
                         $found = false;
                     }
                 }
