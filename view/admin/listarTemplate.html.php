c3930f1f393f5eca4c3304ab088e6d376e194b37
aprendiz_sistemas <aprendiz_sistemas@S10APRSIS1>
2015-09-24 16:08:47 -0500

    modificaciones en la vista listar adm

--- view/admin/listarTemplate.html.php
+++ view/admin/listarTemplate.html.php
@@ -1,5 +1,4 @@
 <?php
-
 use mvc\routing\routingClass as routing ?>
 <?php
 use mvc\view\viewClass as view ?>
@@ -11,28 +10,23 @@ use mvc\i18n\i18nClass as i18n ?>
 <?php $codigo_plan = clienteTableClass::CODIGO_PLAN ?>
 <?php $nombre_plan = clienteTableClass::NOMBRE_PLAN ?>
 <?php $sedes = clienteTableClass::SEDES_ATENCION ?>
-<?php $observaciones = clienteTableClass::OBSERVACIONES ?>
+<?php $observaciones = condicionesTableClass::OBSERVACIONES ?>
 <?php $copago = clienteTableClass::COPAGO ?>
 <?php $carnet = clienteTableClass::BOOL_CARNET ?>
 <?php //$autorizacion = clienteTableClass::CONTROL_AUTORIZACION  ?>
-<!--<link rel="stylesheet" href="../../web/css/jquery.cleditor.css" />
-<script src="../../web/js/jquery.cleditor.min.js"></script>-->
 <?php view::includePartial('default/menuPrincipal') ?>
-  <div class="container">
-<form action="<?php echo routing::getInstance()->getUrlWeb('admin', 'update') ?>"  enctype="multipart/form-data" method="POST">
+<div class="container">
+    <form action="<?php echo routing::getInstance()->getUrlWeb('admin', 'update') ?>"  enctype="multipart/form-data" method="POST">
 
-    <?php if (isset($objListar) == true): ?>
-        <input name="<?php echo clienteTableClass::getNameField(clienteTableClass::CLIENTE_CODIGO, true) ?>" value="<?php echo $objListar[0]->$id ?>" type="hidden">
-    <?php endif ?>
- 
+        <?php if (isset($objListar) == true): ?>
+            <input name="<?php echo clienteTableClass::getNameField(clienteTableClass::CLIENTE_CODIGO, true) ?>" value="<?php echo $objListar[0]->$id ?>" type="hidden">
+        <?php endif ?>
        
-
         <div style="margin-bottom: 10px;">
             <a href="<?php echo routing::getInstance()->getUrlWeb('admin', 'index') ?>" class="btn btn-success btn-xs">INICIO</a>
         </div>
         <div id="container-main">
  
-
             <div class="accordion-container">
                 <p href="#" class="accordion-titulo">Informacion Cliente<span class="toggle-icon"></span></p>
                 <div class="accordion-content" id="cliente">
@@ -52,9 +46,13 @@ use mvc\i18n\i18nClass as i18n ?>
                         <div class="form-group">
                             <label>Código del plan</label>
                             <input id="selectbasic"  class="form-control" name="" value="<?php echo ((isset($objListar) == true) ? $objListar[0]->$codigo_plan : '') ?>"  readonly/>
-
+                                
                         </div>
-
+                        
+                        <div class="form-group">
+                            <label>Autorización</label>
+                            <input type="text" class="form-control" name=""   value="<?php echo (isset($objListar) == true) ? "Telefono: " . $objListar[0]->telefono . "  " : '' ?><?php echo (isset($objListar) == true) ? "E-MAIL/PAGINA WEB: " . $objListar[0]->email_web . "\n" : '' ?>" readonly/>
+                        </div>
                     </div>
 
                     <div class="col-md-6" id="columna2">
@@ -76,34 +74,21 @@ use mvc\i18n\i18nClass as i18n ?>
                         </div>
 
                          <div class="form-group">
-                            <label>Unidad de Negocio</label>
-                           
-                            
-                            
-                            
-<!--                           <input type="text" class="form-control" name="unidaddenegcio"/>-->
-                        
-                         <select class="form-control" id="" name="id_negocio" required>
-                <option value="">Unidad de negocio</option>
-                <?php foreach ($ObjUnidad as $dato): ?> 
-                  <option value="<?php echo $dato->id_unidad_negocio ?>"><?php echo $dato->nombre_unidad ?></option>
-                <?php endforeach ?>
-              </select>
-                         
-                         
-                        
-                         
+                            <label>Unidad de Negocio</label>                       
+                            <select class="form-control" id="" name="id_negocio" required>
+                                <option value="">Unidad de negocio</option>
+                                <?php foreach ($ObjUnidad as $dato): ?> 
+                                    <option value="<?php echo $dato->id_unidad_negocio ?>"><?php echo $dato->nombre_unidad ?></option>
+                                <?php endforeach ?>
+                            </select>
                          </div>
-                        
-                        
+
                     </div>
 
                     <div class="col-md-12" id="columna3">
 
                         <div class="form-group">
-                            <label>Autorización</label>
-                            <p><?php echo (isset($objListar) == true) ? "Telefono: " . $objListar[0]->telefono . "\n" : '' ?></p><p><?php echo (isset($objListar) == true) ? "E-MAIL/PAGINA WEB: " . $objListar[0]->email_web . "\n" : '' ?></p>
-                            <input type="file" name="<?php echo clienteTableClass::getNameField(clienteTableClass::IMAGENES, true)?>" />
+                            <input type="file" name="<?php echo clienteTableClass::getNameField(clienteTableClass::IMAGENES, true) ?>" />
                             <br>
                             <input type="file" name="imagenClienteDos" />
                             <br>
@@ -115,7 +100,6 @@ use mvc\i18n\i18nClass as i18n ?>
                         </div>
                        
                     </div>
-
                 </div>
             </div>
 
@@ -127,29 +111,17 @@ use mvc\i18n\i18nClass as i18n ?>
 
                         <div class="form-group">
                             <label>Historia clinica</label>
-                            
                             <select class="form-control" id="" name="hist_clinica" required>
-                <option value="">Historia Clinica</option>
-                
-                  <option value="TRUE">SI</option>
-                  <option value="FALSE">NO</option>
-               
-              </select>
-                            
-                            
+                                <option value="">Historia Clinica</option>
+                                <option value="TRUE">SI</option>
+                                <option value="FALSE">NO</option>
+                            </select>
                         </div>
 
                         <div class="form-group">
                             <label>Médico Adscrito</label>
-
-
-
-<!--                                <input type="text" class="form-control" name="medico" value="<?php //echo ((isset($objMedico) == true) ? $objMedico[0]->nombre : '')  ?>"/>-->
-
                             <input type="text" class="form-control" name="" value="<?php echo ((empty($objMedico) == true) ? "NO" : "SI") ?>" readonly/>
-
                         </div>
-
 
                         <div class="form-group">
                             <label>Copago</label>
@@ -163,50 +135,31 @@ use mvc\i18n\i18nClass as i18n ?>
                         <div class=" hero-unit form-group">
                             <label>Firma del paciente</label>
                             <select class="form-control" id="" name="firma" required>
-                <option value="">Firma del paciente</option>
-                
-                  <option value="TRUE">SI</option>
-                  <option value="FALSE">NO</option>
-               
-              </select>
-                        
-                        
+                                <option value="">Firma del paciente</option>
+                                <option value="TRUE">SI</option>
+                                <option value="FALSE">NO</option>
+                            </select>
                         </div>
 
                         <div class="form-group">
                             <label>Copia de resultados</label>
-                            
-                            
-                            
-                            
-                             <select class="form-control" id="" name="copia_res" required>
-                <option value="">Copia Resultado</option>
-                
-                  <option value="TRUE">SI</option>
-                  <option value="FALSE">NO</option>
-               
-              </select>
-                        
-                            
-                            
+                            <select class="form-control" id="" name="copia_res" required>
+                                <option value="">Copia Resultado</option>
+                                <option value="TRUE">SI</option>
+                                <option value="FALSE">NO</option>
+                            </select>
                         </div>
 
                         <div class="form-group">
                             <label>Formato No POS</label>
-                            
                             <select class="form-control" id="" name="no_pos" required>
-                <option value="">Formato no POS</option>
-                
-                  <option value="TRUE">SI</option>
-                  <option value="FALSE">NO</option>
-               
-              </select>
-                        
-                        
+                                <option value="">Formato no POS</option>
+                                <option value="TRUE">SI</option>
+                                <option value="FALSE">NO</option>
+                            </select>
                         </div>
 
                     </div>
-
                 </div>
             </div>
 
@@ -216,33 +169,24 @@ use mvc\i18n\i18nClass as i18n ?>
 
                     <div class="col-md-12" id="columna1">
 
-                       
-
                         <div class="form-group">
-                            
                             <label for="observaciones">Observaciones:</label>
                             <textarea id="observ" name="observaciones" class="form-control" rows="5"><?php echo ((isset($objListar) == true) ? $objListar[0]->$observaciones : '') ?>"</textarea>
                         </div>
 
                         <div class="form-group">
                             <center>
-                                <button class="btn btn-lg btn-success btn-signin" style="width: 200px;" type="submit" id="registrar" >
-                                    Registrar</button></center>
+                                <button class="btn btn-lg btn-success btn-signin" style="width: 200px;" type="submit" id="registrar">Registrar</button></center>
                             <input type="hidden" name="hidden" value="1">
                         </div>
 
                     </div>
                 </div>
             </div>
-
         </div>
-
-
 </form>
-
-    </div>
-
-    <script>
+</div>
+<script>
         $(document).ready(function(){
             
            $("#cliente").slideDown();
@@ -264,4 +208,4 @@ use mvc\i18n\i18nClass as i18n ?>
                 $(this).removeClass("open");
             }
         });
-    </script>
+</script>
