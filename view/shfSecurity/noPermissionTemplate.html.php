f38e3d1860d3812e0c7698b19fc5de0768b25599
aprendiz_sistemas <aprendiz_sistemas@S10APRSIS1>
2015-10-13 13:32:34 -0500

    modificacion vista error de permisos

--- view/shfSecurity/noPermissionTemplate.html.php
+++ view/shfSecurity/noPermissionTemplate.html.php
@@ -1,6 +1,7 @@
 <?php
 use mvc\routing\routingClass as routing;
-?>
-
-
-usted no tiene permisos para entrar aquí<a href="<?php echo routing::getInstance()->getUrlWeb('shfSecurity', 'index')?>">click</a>
+?>   
+<h1>403</h1>
+<p>Error de permisos</p>
+<a class="button" href="<?php echo routing::getInstance()->getUrlWeb('default','index')?>">
+    <i class="glyphicon glyphicon-home"></i>  Volver a la pagina de inicio</a>
