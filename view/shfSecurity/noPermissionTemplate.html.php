dd35ba89c07e3bf8f6ed6db93a28f30c1f9caefe
aprendiz_sistemas <aprendiz_sistemas@S10APRSIS1>
2015-10-13 17:11:07 -0500

    cambio msj error 403

--- view/shfSecurity/noPermissionTemplate.html.php
+++ view/shfSecurity/noPermissionTemplate.html.php
@@ -1,7 +1,7 @@
 <?php
 use mvc\routing\routingClass as routing;
 ?>   
-<h1>403</h1>
-<p>Error de permisos</p>
+<h1>Error 403</h1>
+<p>Usted no tiene permisos para estar aqui,por favor comuniquese con el administrador de la plataforma</p>
 <a class="button" href="<?php echo routing::getInstance()->getUrlWeb('default','index')?>">
     <i class="glyphicon glyphicon-home"></i>  Volver a la pagina de inicio</a>
